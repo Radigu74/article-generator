74781dac0f9f90def421876a1622cbb11ceb5f5b
Radigu74 <radigu74@yahoo.com>
2025-05-08 15:57:29 +0800

    Update crud.py

--- backend/crud.py
+++ backend/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from schemas import Topic, Article
+from schemas import TopicRequest, ArticleRequest  # ✅
 
 def get_client_by_key(db: Session, key: str):
     return db.query(Client).filter(Client.api_key == key).first()
